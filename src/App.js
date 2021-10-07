0c75bdbbe7b06101aff380476b3c5e3d1ff226c9
Mubeen Khan <63047730+i170071MubeenKhan@users.noreply.github.com>
2021-10-07 18:29:27 +0500

    basic-todo-app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text="Basic React" />
+      <Todo text="Intermediate React" />
+      <Todo text="Expert React" />
+    </div>
+  );
+}
+
+export default App;
